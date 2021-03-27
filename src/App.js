ec2a5cf7ed1c372939e4ec5a6c2f905b8abac211
bhargavannem <bhargav.annem@gmail.com>
2021-03-26 23:57:26 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navigation'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Parallax, Background } from 'react-parallax';
+
 function App() {
+  const Container = () => (
+    <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={require('./temp_picture.jpg')}
+        bgImageAlt="the dog"
+        strength={-200}
+    >
+        Blur transition from min to max
+        <div style={{ height: '200px' }} />
+    </Parallax>
+);
   return (
     <div className="App">
       <Navbar />
+     {Container}
     </div>
   );
 }
